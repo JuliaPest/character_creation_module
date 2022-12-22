cbb6f19eb856edebd0b2d266f1a0f34be97db58e
Юлия Пестова <juliapest@yandex.ru>
2022-12-22 03:26:05 +0300

    code according to PEP257– Docstring Conventions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack function."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 f' нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -25,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Choosing a skills."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 f' применил специальное умение «Выносливость {80 + 25}»')
@@ -36,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Start training function."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choice char class function."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -82,7 +88,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +99,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
